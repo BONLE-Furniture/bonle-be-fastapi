1467ec45b6f03203cddd7b4c6d184abbac2752e4
uksungbae <qorlwk324@naver.com>
2025-01-18 01:31:31 +0900

    feat: 판매처 링크 조회 기능 추가

--- run.py
+++ run.py
@@ -20,3 +20,22 @@ async def get_product(product_id: str):
         product["_id"] = str(product["_id"])
         return product
     raise HTTPException(status_code=404, detail="항목을 찾을 수 없습니다.")
+
+
+
+# 판매처 링크 조회 API
+@app.get("/product/{product_id}/shop-urls")
+async def get_shop_urls(product_id: str):
+
+    product = await db["bonre_products"].find_one({"_id": ObjectId(product_id)})
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    shop_urls = product.get("shop_urls", [])
+    if not shop_urls:
+        raise HTTPException(status_code=404, detail="No shop URLs found for this product")
+
+    # 반환할 데이터 정리
+    result_data = [{"shop_id": shop["shop_id"], "url": shop["url"]} for shop in shop_urls]
+
+    return  result_data

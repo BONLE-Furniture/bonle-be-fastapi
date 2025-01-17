bfc605f85a74c3e0d0412fd885a31f2a94e18bfa
uksungbae <qorlwk324@naver.com>
2025-01-18 01:31:01 +0900

    fix: 주석문 제거

--- database.py
+++ database.py
@@ -13,5 +13,3 @@ db_uri = os.getenv("MONGODB_URI")
 # MongoDB 클라이언트 설정
 client = AsyncIOMotorClient(db_uri, tlsCAFile=certifi.where())
 db = client.bonre
-# product_collection = db.get_collection("bonre_products")
-

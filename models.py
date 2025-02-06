b39fe4628babe5ef9572e5329ad8fce73bba802c
uksungbae <qorlwk324@naver.com>
2025-02-06 19:18:54 +0900

    fix: models.py 수정

--- models.py
+++ models.py
@@ -5,16 +5,17 @@ from datetime import datetime
 from bson import ObjectId
 from enum import Enum
 
-
 """
 Product
 """
+
 
 # Cheapest 모델 (가격 이력)
 class Product_Cheapest(BaseModel):
     date: datetime
     price: int
     shop_id: str
+
 
 # Size 모델 (size 정보)
 class Product_Size(BaseModel):
@@ -23,11 +24,13 @@ class Product_Size(BaseModel):
     depth: Optional[float] = None
     length: Optional[float] = None
 
+
 # ShopUrl 모델 (상점 URL)
 class Product_ShopUrl(BaseModel):
     shop_id: str
     url: HttpUrl
     priceCC: bool = False
+
 
 # Product update용 모델
 class Product(BaseModel):
@@ -39,17 +42,17 @@ class Product(BaseModel):
     designer: Optional[List[str]] = []
     color: str
     size: Product_Size
-
-    description: Optional[str] =""
-    material: Optional[str] =""
+    description: Optional[str] = ""
+    material: Optional[str] = ""
     filter: Optional[object]  # 색상과 재질 필터
-    category: Optional[str] =""
+    category: Optional[str] = ""
     bookmark_counts: Optional[int] = 0
     shop_urls: List[Product_ShopUrl]  # 각 상점 URL 정보
     main_image_url: Optional[str] = ""  # 이미지 URL
-    cheapest: Optional[List[Product_Cheapest]] = []# 가격 이력 리스트
+    cheapest: Optional[List[Product_Cheapest]] = []  # 가격 이력 리스트
     brand_kr: str
-    upload : bool = False
+    upload: bool = False
+
 
 # Product update용 모델
 class ProductUpdate(BaseModel):
@@ -63,14 +66,15 @@ class ProductUpdate(BaseModel):
     size: Optional[Product_Size] = None
     description: Optional[str] = None
     material: Optional[str] = None
-    filter: Optional[object] = None # 색상과 재질 필터
+    filter: Optional[object] = None  # 색상과 재질 필터
     category: Optional[str] = None
     bookmark_counts: Optional[int] = None
-    shop_urls: List[Product_ShopUrl] = None # 각 상점 URL 정보
+    shop_urls: List[Product_ShopUrl] = None  # 각 상점 URL 정보
     main_image_url: Optional[str] = None  # 이미지 URL
-    cheapest: Optional[List[Product_Cheapest]] = None# 가격 이력 리스트
+    cheapest: Optional[List[Product_Cheapest]] = None  # 가격 이력 리스트
     brand_kr: Optional[str] = None
-    upload : Optional[bool] = None
+    upload: Optional[bool] = None
+
 
 class Product_Period(str, Enum):
     one_week = "1week"
@@ -78,9 +82,12 @@ class Product_Period(str, Enum):
     one_year = "1year"
     all_time = "all"
 
+
 """
 Brand
 """
+
+
 class Brand(BaseModel):
     id: str = Field(alias="_id")
     brand_kr: str
@@ -88,19 +95,21 @@ class Brand(BaseModel):
     comment: Optional[str] = None
     bookmark_count: Optional[int] = 0
     main_image_url: str
-    
+
     class Config:
         allow_population_by_field_name = True
 
-# Brand 생성 모델
+    # Brand 생성 모델
+
+
 class BrandUpdate(BaseModel):
     brand_kr: Optional[str] = None
     brand: Optional[str] = None
     comment: Optional[str] = None
     bookmark_count: Optional[int] = None
     main_image_url: Optional[str] = None
-    
-    
+
+
 class Shop(BaseModel):
     id: str = Field(alias="_id")
     shop_kr: str
@@ -123,9 +132,10 @@ class ShopUpdate(BaseModel):
     link: Optional[str] = None
     sld: Optional[str] = None
     brand_list: Optional[List[str]] = []
-    
+
     class Config:
-        allow_population_by_field_name = True  
+        allow_population_by_field_name = True
+
 
 class Designer(BaseModel):
     id: str = Field(alias="_id")
@@ -137,6 +147,7 @@ class Designer(BaseModel):
     class Config:
         allow_population_by_field_name = True
 
+
 class DesignerUpdate(BaseModel):
     designer_kr: Optional[str] = None
     designer: Optional[str] = None
@@ -146,25 +157,29 @@ class DesignerUpdate(BaseModel):
     class Config:
         allow_population_by_field_name = True
 
+
 class Price(BaseModel):
     date: datetime
     price: int
 
+
 class Product_Price(BaseModel):
     product_id: str
-    color : str
-    brand_id : str
-    shop_sld : str
-    prices : List[Price]
-    
+    color: str
+    brand_id: str
+    shop_sld: str
+    prices: List[Price]
+
+
 class bookmark(BaseModel):
     id: str = None
-    userId:str
+    userId: str
     product_id: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
-    __v : Optional[int] = 0
-    
+    __v: Optional[int] = 0
+
+
 # Object Type to STR변환
 def sanitize_data(data):
     sanitized_data = []
@@ -178,9 +193,9 @@ def sanitize_data(data):
                     sanitized_item[key] = None
                 else:
                     sanitized_item[key] = value
-            elif isinstance(value, dict): 
+            elif isinstance(value, dict):
                 sanitized_item[key] = sanitize_data([value])[0]
-            elif isinstance(value, list): 
+            elif isinstance(value, list):
                 sanitized_item[key] = [
                     str(v) if isinstance(v, ObjectId)
                     else sanitize_data([v])[0] if isinstance(v, dict)
